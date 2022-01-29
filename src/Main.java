105bca6f51268ee5f86db0b1dd48fb057c81d1da
Олеся Трофимова <trofa811@gmail.com>
2022-01-29 19:48:53 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Решение №1");
+        System.out.println("Решение 1");
+        System.out.println("Решение 2");
+        System.out.println("Решение 3");
     }
 }
